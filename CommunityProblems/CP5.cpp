6efb221e83a4768966f5ac87c2d94e727d3f4d44
ReetzDev <131424020+ReetzDev@users.noreply.github.com>
2023-05-27 23:21:48 +0530

    CP5.cpp

---
+++ CommunityProblems/CP5.cpp
@@ -0,0 +1,22 @@
+//https://www.codechef.com/problems/FEVER
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,X;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X;
+	    if(X>98)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
