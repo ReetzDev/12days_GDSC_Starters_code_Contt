33a2400246c802647472eae68a4d9a9a5b33f85a
ReetzDev <131424020+ReetzDev@users.noreply.github.com>
2023-05-27 23:20:55 +0530

    CP4.cpp

---
+++ CommunityProblems/CP4.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/problems/BTRYHLTH
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,X;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X;
+	    if(X>=80)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
